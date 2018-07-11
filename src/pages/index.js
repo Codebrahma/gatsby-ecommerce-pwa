d0c5ac46d68e3b384cfbe93cfaba51b880fb159d
prasanna1211 <prasanna@codebrahma.com>
2018-07-11 13:19:38 +0530

    initial scss commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,75 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+import styles from '../components/css/custom.scss';
+
 const IndexPage = () => (
   <div>
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <Link to="/page-2/">Go to page 2</Link>
+  <div className="nav-container">
+    <div className="nav-inner">
+      <div id="pt_custommenu" className="pt_custommenu">
+        <div id="pt_menu" className="pt_menu">
+          <div className="parentMenu">
+            <a className="fontcustom2" href="/pos_nevara/layout3/">
+              <span>Men</span>
+            </a>
+          </div>
+        </div>
+        <div id="pt_menu" className="pt_menu act nav-1">
+          <div className="parentMenu">
+            <a href="http://demo.posthemes.com/pos_nevara/layout3/en/10-shop" className="fontcustom2">
+              <span>Women</span>
+              <i className="icon-caret-down"></i>
+            </a>
+          </div>
+        </div>
+        <div id="pt_menu" className="pt_menu">
+          <div className="parentMenu">
+            <a className="fontcustom2" href="/pos_nevara/layout3/">
+              <span>Furniture</span>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
+  <div className="header-menu">
+  <div className="ma-nav-mobile-container hidden-lg-up">
+  <div className="navbar">
+    <div id="navbar-inner" className="navbar-inner navbar-inactive">
+    <a className="btn-navbar">Category</a>
+    <ul id="pt_custommenu_itemmobile" className="tree dhtml  mobilemenu nav-collapse collapse">
+      <li>
+        <a href="http://demo.posthemes.com/pos_nevara/layout3/en/10-shop">Shop </a>
+        <ul className="dhtml">
+          <li>
+            <a href="http://demo.posthemes.com/pos_nevara/layout3/en/13-art-gallery">Art Gallery </a>
+            <ul>
+              <li>
+                <a href="http://demo.posthemes.com/pos_nevara/layout3/en/16-document">Document </a>
+              </li>
+                <li><a href="http://demo.posthemes.com/pos_nevara/layout3/en/17-dropcap">Dropcap </a>
+              </li>
+              <li>
+                <a href="http://demo.posthemes.com/pos_nevara/layout3/en/18-dummy-image">Dummy Image </a>
+              </li>
+              <li>
+                <a href="http://demo.posthemes.com/pos_nevara/layout3/en/19-dummy-text">Dummy Text </a>
+              </li>
+              <li>
+                <a href="http://demo.posthemes.com/pos_nevara/layout3/en/20-fancy-text">Fancy Text </a>
+              </li>
+            </ul>
+              <span className="grower"><a href="javascript:void(0)"></a></span>
+          </li>
+        </ul>                                            
+      </li>  
+    </ul>
+  </div>
+</div>
+</div>
+</div>
+</div>
 )
 
 export default IndexPage
