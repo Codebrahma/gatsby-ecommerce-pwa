99e71b98e40cf09f4a9ae8fcd4e92025f02fedf9
navneet <navneet@codebrahma.com>
2018-07-12 14:22:41 +0530

    Added Prettier

--- src/layouts/NavbarMobile.js
+++ src/layouts/NavbarMobile.js
@@ -1,32 +1,33 @@
 import React, { Component } from 'react'
 import Menu from '../components/menu.js'
 
-
 class NavbarMobile extends Component {
   state = {
     displaySubMenu: false,
   }
 
- mainMenuClick = (e)=> {
-  this.setState((prevState) => ({
+  mainMenuClick = e => {
+    this.setState(prevState => ({
       displaySubMenu: !prevState.displaySubMenu,
-  }))
-}
-render() {
-  const { displaySubMenu } = this.state;
-  return (
-    <div className="ma-nav-mobile-container hidden-lg-up">
-      <div className="pt_custommenu_mobile">
-        <div className="navbar">
-          <div id="navbar-inner" className="navbar-inner navbar-inactive">
-            <span className="btn-navbar" onClick={this.mainMenuClick}>Category</span>
-            {displaySubMenu && <Menu />}
+    }))
+  }
+  render() {
+    const { displaySubMenu } = this.state
+    return (
+      <div className="ma-nav-mobile-container hidden-lg-up">
+        <div className="pt_custommenu_mobile">
+          <div className="navbar">
+            <div id="navbar-inner" className="navbar-inner navbar-inactive">
+              <span className="btn-navbar" onClick={this.mainMenuClick}>
+                Category
+              </span>
+              {displaySubMenu && <Menu />}
+            </div>
           </div>
         </div>
       </div>
-    </div>
-  )
-}
+    )
+  }
 }
 
 export default NavbarMobile
