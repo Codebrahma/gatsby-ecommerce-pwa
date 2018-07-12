3b985f08655e12673f0706378da2850dceeacdf9
prasanna1211 <prasanna@codebrahma.com>
2018-07-12 17:42:44 +0530

    changed side bar

--- src/components/presentational/products/SideBarItem.js
+++ src/components/presentational/products/SideBarItem.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const SideBarItem = ({ title }) => (
   <li>
-    <label className="facet-label">
+    <label className="facet-label" for="facet_input_16268_0">
       <span className="custom-checkbox">
         <input
           id="facet_input_20275_1"
