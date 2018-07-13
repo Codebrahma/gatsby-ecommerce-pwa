b828c4ffe94203dd19eb0486751366fbc914fa3b
navneet <navneet@codebrahma.com>
2018-07-13 11:59:15 +0530

    gatsby develop bug skip

--- gatsby-config.js
+++ gatsby-config.js
@@ -11,13 +11,6 @@ module.exports = {
         precision: 8,
       },
     },
-    {
-      resolve: 'gatsby-source-shopify',
-      options: {
-        shopName: 'ecommerce-codebrahma',
-        accessToken: 'c9e65f71dc1b5c80fad0dacacc7485f2',
-        verbose: true,
-      },
-    },
+
   ],
 }
