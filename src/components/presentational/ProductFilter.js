5ee35f723bc161e61d5892593f2602522b7c7359
sai <saikrishna@codebrahma.com>
2018-07-14 09:56:34 +0530

    Filter Categories Component refactored

--- src/components/presentational/ProductFilter.js
+++ src/components/presentational/ProductFilter.js
@@ -1,53 +1,17 @@
 import React, { Component } from 'react'
-import CustomCheckbox from './CustomCheckbox'
-import map from 'lodash'
-import './filter.scss'
+import FilterBy from './FilterBy'
+
 
 const sizes = [10, 11, 12, 13, 14]
 const colors = ['red', 'green', 'blue', 'pink', 'yellow']
 
-class ProductFilter extends Component {
-  handleChange = e => {
-    console.log(e.target.value)
-  }
-  render() {
-    return (
-      <div className="container">
-        <div className="row">
-          <div className="filterBy ">
-            <h4>SIZE</h4>
-            {_.map(sizes,size => (
-              <div key={size}>
-                <CustomCheckbox
-                  filter={size}
-                  id={`filterBySize${size}`}
-                  name="size"
-                  handleChange={this.handleChange}
-                />
-                <div className="color-box">{size}</div>
-              </div>
-            ))}
-          </div>
-          <div className="filterBy">
-            <h4>COLOR</h4>
-            {_.map(colors,color => (
-              <div key={color}>
-                <CustomCheckbox
-                  filter={color}
-                  id={`filterBySize${color}`}
-                  name="color"
-                  handleChange={this.handleChange}
-                />
-                <div className={`color-box ${color}`}>
-                  {color.slice(0, 1).toUpperCase()}
-                </div>
-              </div>
-            ))}
-          </div>
-        </div>
-      </div>
-    )
-  }
-}
+const ProductFilter = () => (
+  <div className="container">
+    <div className="row">
+      <FilterBy label="size" filterWhat={sizes} />
+      <FilterBy label="color" filterWhat={colors} />
+    </div>
+  </div>
+)
 
 export default ProductFilter
