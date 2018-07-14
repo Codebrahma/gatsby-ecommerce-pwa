8e70b273ba1e0ad12e0cfd905b7fea56d15f84f5
prasanna1211 <prasanna@codebrahma.com>
2018-07-14 22:27:39 +0530

    fixed conflicts:

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,16 +1,10 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Header from './header'
-<<<<<<< HEAD
-import './index.css'
-import '../components/css/custom.scss'
-import 'bootstrap/dist/css/bootstrap.min.css'
-=======
 import Helmet from 'react-helmet';
 import './index.scss'
 import '../components/css/custom.scss';
 import 'bootstrap/dist/css/bootstrap.min.css';
->>>>>>> added home page
 
 const Layout = ({ children }) => (
   <div>
