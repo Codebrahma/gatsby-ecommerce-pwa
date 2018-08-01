369d117d9156243f4089dcf4fa31456b1043fd90
sai <saikrishna@codebrahma.com>
2018-08-02 00:10:18 +0530

    Initial UI

---
+++ src/utils/dummyData.js
@@ -0,0 +1,14 @@
+export const products = [
+    {
+        title: "THE LEAN MACHINE KETO PROGRAM: 7 DAY SUBSCRIPTION",
+        image: require('./images/GrowFit_1.jpg'),
+        price: "2310.00",
+        tags: [
+            "Keto/LCHF Friendly",
+            "Increased Energy",
+            "Better Sleep",
+            "Reduce Fatty Liver",
+            "Youthful Skin"
+        ]
+    }
+]
