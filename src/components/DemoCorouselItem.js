c0dc88d41dd6a532b0c4c3fa30bde2de4287946c
sai <kskprasad96@gmail.com>
2018-08-06 23:45:17 +0530

    home page carousel and subscription steps css bugs fixed

--- src/components/DemoCorouselItem.js
+++ src/components/DemoCorouselItem.js
@@ -3,9 +3,9 @@ import  Link from 'gatsby-link'
 import Img from 'gatsby-image';
 
 const DemoCorouselItem = (props) => (
-  <div className={`carousel-item active`}>
-    <Link to={`product/${props.productId}`} >
-      <Img className="d-block w-100" sizes={props.image} alt="home-page-item"/>
+  <div className="carousel-item active">
+    <Link to={`product/${props.productId}`} style={{margin: "0"}}>
+      <Img className="d-block w-100 demo-carousel-image" sizes={props.image} alt="home-page-item"/>
     </Link>
   </div>
 )
