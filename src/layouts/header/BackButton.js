69bec3a6a85e14218a5bdbac3b9d705c476042eb
sai <kskprasad96@gmail.com>
2018-08-13 10:17:33 +0530

    Back button removed on Home page

--- src/layouts/header/BackButton.js
+++ src/layouts/header/BackButton.js
@@ -3,21 +3,14 @@ import { withRouter } from 'react-router'
 
 import back from '../../assets/icons/arrow-left-solid.svg'
 
-const BackButton = (props) => {
-  const goBack = () => {
-    if(props.location.pathname !== '/') {
-      props.history.goBack()
-    }
-  }
-  return (
+const BackButton = (props) =>  (
     <div 
-        onClick={goBack} 
+        onClick={() => props.history.goBack()} 
         style={{marginRight: "10px", cursor: "pointer"}}
     >
       <img src={back} alt="go back" className="icon icon-mobile"/>
     </div>
   )
-}
 
 const BackButtonWithRouter = withRouter(BackButton)
 
