e68355b1ef6c93c62947099df84c78cedd02ee77
Arjun <snarjun92@gmail.com>
2018-08-14 12:11:18 +0530

    use npm link to link theme packages to project

--- theme-path.js
+++ theme-path.js
@@ -2,5 +2,11 @@ const path = require('path')
 const detectInstalled = require('detect-installed')
 const config = require('./config.json')
 
-let themePath = path.resolve(__dirname, './themes/' + config.theme)
+if (detectInstalled.sync(config.theme, { local: true })) {
+  themePath = path.resolve(__dirname, './node_modules/' + config.theme)
+}
+else {
+  themePath = path.resolve(__dirname, './themes/' + config.theme)
+}
+
 module.exports = themePath
